5b04610af248239daf24bce74dee1852aa5b33df
mohammadiriaz <mohammadr95@nycstudents.net>
2025-11-06 12:31:53 -0500

    Update APCalendar.java

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+public class APCalendar
+{
 
-public class APCalendar {
+    private static boolean isLeapYear(int year)
+    {
+        if (year % 4 == 0 && year % 100 != 0)
+        {
+        return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public static int numberOfLeapYears(int year1, int year2)
+    {
+
+    }
+
+    private static int firstDayOfYear(int year)
+    {
+
+    }
+
+    private static int dayOfYear(int month, int day, int year)
+    {
+
+    }
+
+    public static int dayOfWeek(int month, int day, int year)
+    {
+
+    }
 }
