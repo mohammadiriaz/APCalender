1c49ff8f8d4c6ae8126c5871bd5a1d42a743067c
mohammadiriaz <mohammadr95@nycstudents.net>
2025-11-10 12:26:37 -0500

    Finished with the test cases.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(APCalendar.numberOfLeapYears(2000, 2025));
+        System.out.println(APCalendar.dayOfWeek(1,10,2019));
     }
 }
